5c4fb1ab5aad9d5cf488769f516291c62c444d2c
nickgric <nick.gric@gmail.com>
2022-12-04 21:59:30 +0300

    final

--- src/components/AddContact/AddContact.jsx
+++ src/components/AddContact/AddContact.jsx
@@ -33,7 +33,7 @@ export class AddContact extends Component {
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan!"
               required
             />
             <b>→ name</b>
@@ -44,7 +44,7 @@ export class AddContact extends Component {
               type="tel"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +!"
               required
             />
             <b>→ number</b>
